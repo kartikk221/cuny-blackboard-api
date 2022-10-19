d5d0e7a2b3b2fce7d5f03a3f48d20cf757be1f73
kartikk221 <kartikk221@yahoo.com>
2022-10-19 16:34:38 -0400

    Implemented constant error codes

--- src/modules/blackboard/shared.ts
+++ src/modules/blackboard/shared.ts
@@ -1,8 +1,9 @@
-// Define constants
 export const URL_BASE = 'https://bbhosted.cuny.edu';
 export const USER_AGENT =
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36';
-export const SESSION_COOKIES = [
-    'JSESSIONID',
-    'BbRouter',
-];
+export const SESSION_COOKIES = ['JSESSIONID', 'BbRouter'];
+export const ERROR_CODES = {
+    UNAUTHORIZED: 'BLACKBOARD_API_UNAUTHORIZED',
+    NOT_FOUND: 'BLACKBOARD_API_NOT_FOUND',
+    SERVER_ERROR: 'BLACKBOARD_API_SERVER_ERROR',
+};
--- src/routes/handlers.ts
+++ src/routes/handlers.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'hyper-express';
+import { ERROR_CODES } from '../modules/blackboard/shared';
 
 export function not_found_handler(_: Request, response: Response) {
     response.status(404).json({
@@ -8,17 +9,27 @@ export function not_found_handler(_: Request, response: Response) {
 }
 
 export function error_handler(_: Request, response: Response, error: Error) {
-    // Handle the known blackboard api unauthorized error
-    if (error.message === 'BLACKBOARD_API_UNAUTHORIZED')
-        return response.status(401).json({
-            code: 'INVALID_TOKEN',
-            message: `The provided token is invalid or has expired.`,
-        });
+    // Handle known errors with custom responses
+    switch (error.message) {
+        case ERROR_CODES.UNAUTHORIZED:
+            return response.status(401).json({
+                code: 'INVALID_TOKEN',
+                message: `The provided token is invalid or has expired.`,
+            });
+        case ERROR_CODES.NOT_FOUND:
+            return response.status(404).json({
+                code: 'NOT_FOUND',
+                message: 'The requested resource was not found on Blackboard.',
+            });
+        case ERROR_CODES.SERVER_ERROR:
+            return response.status(500).json({
+                code: 'BLACKBOARD_ERROR',
+                message: 'An error occurred while communicating with Blackboard. Please try again later.',
+            });
+    }
 
-    // Log the error to the console
+    // Log the uncaught error to the console and return a generic error response
     console.error(error);
-
-    // Send an error response
     response.status(500).json({
         code: 'SERVER_ERROR',
         message: 'An unexpected error occurred on the server.',
